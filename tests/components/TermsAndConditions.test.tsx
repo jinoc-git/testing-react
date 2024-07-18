87b65c66c23f9d12574610dec903c6b953f2cb47
jinoc <choiae369@gmail.com>
2024-07-19 00:06:42 +0900

    TermsAndConditions 테스트 컴포넌트 리팩토링

--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -3,29 +3,32 @@ import TermsAndConditions from '../../src/components/TermsAndConditions';
 import userEvent from '@testing-library/user-event';
 
 describe('TermsAndConditions', () => {
-  it('should render with correct text ans initial state', () => {
+  // 이 함수로 중복 코드 방지
+  const renderComponent = () => {
     render(<TermsAndConditions />);
 
-    const heading = screen.getByRole('heading');
-    expect(heading).toBeInTheDocument();
+    // 기존의 toBeInTheDocument는 getByRole이라 없어도됨
+    return {
+      heading: screen.getByRole('heading'),
+      checkbox: screen.getByRole('checkbox'),
+      button: screen.getByRole('button'),
+    };
+  };
+
+  it('should render with correct text ans initial state', () => {
+    const { heading, checkbox, button } = renderComponent();
+
     expect(heading).toHaveTextContent('Terms & Conditions');
-
-    const checkbox = screen.getByRole('checkbox');
-    expect(checkbox).toBeInTheDocument();
     expect(checkbox).not.toBeChecked(); // 체크박스가 체크되지 않은 상태인지
-
-    const button = screen.getByRole('button');
-    expect(button).toBeInTheDocument();
     expect(button).toBeDisabled(); // 버튼이 disabled 상태인지
   });
 
   it('should enable the button when the checkbox is checked', async () => {
-    render(<TermsAndConditions />);
+    const { checkbox, button } = renderComponent();
 
-    const checkbox = screen.getByRole('checkbox');
     const user = userEvent.setup();
     await user.click(checkbox); // 유저 이벤트는 async await를 사용
 
-    expect(screen.getByRole('button')).toBeEnabled(); // 버튼이 enabled가 되는지
+    expect(button).toBeEnabled(); // 버튼이 enabled가 되는지
   });
 });

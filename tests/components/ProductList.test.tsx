54b0c1dff0d45ba298a5b57d1ab07ca709f78639
jinoc <choiae369@gmail.com>
2024-07-27 20:35:36 +0900

    ProductList 컴포넌트 리팩토링
    - 기존 useState, useEffect로 데이터 fetching대신 useQuery 사용
    - ProductList 테스트 컴포넌트도 useQuery에 따른 변경사항 수정

--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -7,6 +7,7 @@ import ProductList from '../../src/components/ProductList';
 import { server } from '../mocks/server';
 import { delay, http, HttpResponse } from 'msw';
 import { db } from '../mocks/db';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 describe('ProductList', () => {
   const productIds: number[] = []; // 생성할 product들의 id들을 할당할 배열
@@ -23,8 +24,21 @@ describe('ProductList', () => {
     db.product.deleteMany({ where: { id: { in: productIds } } });
   });
 
+  // useQuery로 변경했기 때문에 사용
+  const renderComponent = () => {
+    const client = new QueryClient({
+      defaultOptions: { queries: { retry: false } },
+    });
+
+    render(
+      <QueryClientProvider client={client}>
+        <ProductList />
+      </QueryClientProvider>
+    );
+  };
+
   it('should render the list of products', async () => {
-    render(<ProductList />);
+    renderComponent();
 
     const items = await screen.findAllByRole('listitem'); // li 태그 찾아서
     expect(items.length).toBeGreaterThan(0); // 0개 초과인지 확인
@@ -34,7 +48,7 @@ describe('ProductList', () => {
     // mock server에서 /products api로 빈 배열을 res하여 ProductList 컴포넌트에서 data가 빈 배열로 만듦
     server.use(http.get('/products', () => HttpResponse.json([])));
 
-    render(<ProductList />);
+    renderComponent();
 
     const message = await screen.findByText(/no products/i); // products.length가 0일 때 보여지는 text를 찾아서
     expect(message).toBeInTheDocument(); // 화면에 있는지 확인
@@ -43,7 +57,7 @@ describe('ProductList', () => {
   it('should render an error message when there is an error', async () => {
     server.use(http.get('/products', () => HttpResponse.error()));
 
-    render(<ProductList />);
+    renderComponent();
 
     expect(await screen.findByText(/error/i)).toBeInTheDocument();
   });
@@ -56,7 +70,7 @@ describe('ProductList', () => {
       })
     );
 
-    render(<ProductList />);
+    renderComponent();
 
     expect(await screen.findByText(/loading/i)).toBeInTheDocument(); // delay 동안 loading이 보이는지
   });
@@ -64,7 +78,7 @@ describe('ProductList', () => {
   it('should remove the loading indicator after data is fetched', async () => {
     server.use(http.get('/products', () => HttpResponse.error()));
 
-    render(<ProductList />);
+    renderComponent();
 
     // screen.queryByText(/loading/i)는 없을 때는 null, 1개 있으면 return, 여러개면 throw함
     // 만약 null이면 에러 발생하고 loading 텍스트를 갖고 있는 요소가 제거될 때까지 기다림 (통과)

c8cd779a85d198d110b0815e9ab84070827b9a8d
jinoc <choiae369@gmail.com>
2024-07-27 20:25:29 +0900

    ProductDetail 로딩 인디케이터 테스트 케이스 추가

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,7 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import ProductDetail from '../../src/components/ProductDetail';
 import { server } from '../mocks/server';
-import { http, HttpResponse } from 'msw';
+import { delay, http, HttpResponse } from 'msw';
 import { db } from '../mocks/db';
 
 describe('ProductDetail', () => {
@@ -55,5 +59,26 @@ describe('ProductDetail', () => {
     render(<ProductDetail productId={1} />);
 
     expect(await screen.findByText(/error/i)).toBeInTheDocument();
+  });
+
+  it('should render a loading indicator when fetching data', async () => {
+    server.use(
+      http.get('/product/1', async () => {
+        await delay();
+        return HttpResponse.json([]);
+      })
+    );
+
+    render(<ProductDetail productId={1} />);
+
+    expect(await screen.findByText(/loading/i)).toBeInTheDocument();
+  });
+
+  it('should remove the loading indicator if data fetching fails', async () => {
+    server.use(http.get('/product/1', () => HttpResponse.error()));
+
+    render(<ProductDetail productId={1} />);
+
+    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
   });
 });

d6271d4b267e598d094cff775985d7efbaf3a8ac
jinoc <choiae369@gmail.com>
2024-07-29 00:05:20 +0900

    BrowseProductsPage 테스트 컴포넌트에 테스트 케이스 추가
    - 데이터가 정상적으로 렌더링되는지 확인 테스트 추가
    - mocks/db.ts에 category 추가, product에 categoryId 추가
    - mocks/handler.ts에 category mock rest api 추가

--- tests/components/BrowseProductsPage.test.tsx
+++ tests/components/BrowseProductsPage.test.tsx
@@ -7,13 +7,42 @@ import BrowseProducts from '../../src/pages/BrowseProductsPage';
 import { Theme } from '@radix-ui/themes';
 import { server } from '../mocks/server';
 import { delay, http, HttpResponse } from 'msw';
+import { Category, Product } from '../../src/entities';
+import { db } from '../mocks/db';
+import { CartProvider } from '../../src/providers/CartProvider';
+import userEvent from '@testing-library/user-event';
 
 describe('BrowseProductsPage', () => {
+  const categories: Category[] = [];
+  const products: Product[] = [];
+
+  // 카테고리와 상품을 2개씩 생성
+  beforeAll(() => {
+    [1, 2].forEach(() => {
+      categories.push(db.category.create());
+      products.push(db.product.create());
+    });
+  });
+
+  afterAll(() => {
+    // 카테고리 id들을 배열로 만들어서
+    const categoryIds = categories.map((category) => category.id);
+    // db category에 categoryIds에 있는 id와 일치하는 것들을 삭제
+    db.category.deleteMany({ where: { id: { in: categoryIds } } });
+
+    // 상품 id들을 배열로 만들어서
+    const productIds = products.map((product) => product.id);
+    // db product에 categoryIds에 있는 id와 일치하는 것들을 삭제
+    db.product.deleteMany({ where: { id: { in: productIds } } });
+  });
+
   const renderComponent = () => {
     render(
-      <Theme>
-        <BrowseProducts />
-      </Theme>
+      <CartProvider>
+        <Theme>
+          <BrowseProducts />
+        </Theme>
+      </CartProvider>
     );
   };
 
@@ -95,5 +124,39 @@ describe('BrowseProductsPage', () => {
     renderComponent();
 
     expect(await screen.findByText(/error/i)).toBeInTheDocument(); // error란 텍스트가 있는지
+  });
+
+  it('should render categories', async () => {
+    renderComponent();
+
+    // getByRole을 즉시 찾지만 findByRole은 비동기적으로 요소를 찾음
+    // (동적으로 생성되거나 비동기적으로 로딩되는 요소 찾을 때 씀)
+    // 시간이 지나도 요소를 찾지 못하면 TimeoutError를 발생
+    const combobox = await screen.findByRole('combobox');
+    expect(combobox).toBeInTheDocument();
+
+    const user = userEvent.setup();
+    await user.click(combobox);
+
+    // 기본으로 렌더링되는 옵션인 All 옵션이 렌더링 되는지 확인
+    expect(screen.getByRole('option', { name: /all/i })).toBeInTheDocument();
+    // beforeAll에서 생성한 categories를 순회하면서 각 옵션이 잘 렌더링 되는지 확인
+    categories.forEach((category) => {
+      expect(
+        screen.getByRole('option', { name: category.name })
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('should render products', async () => {
+    renderComponent();
+
+    await waitForElementToBeRemoved(() =>
+      screen.getByRole('progressbar', { name: /products/i })
+    );
+
+    products.forEach((product) => {
+      expect(screen.getByText(product.name)).toBeInTheDocument(); // 상품이 렌더링되는지 확인
+    });
   });
 });
--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -3,6 +3,7 @@
 import { db } from './db';
 
 // mock api 설정
+// test코드에서 컴포넌트를 render할 때 api 요청을 msw가 intercept함
 export const handlers = [
   // http.get('/categories', () => {
   //   return HttpResponse.json([
@@ -20,5 +21,7 @@ export const handlers = [
   //   if (!product) return new HttpResponse(null, { status: 404 });
   //   return HttpResponse.json(product);
   // }),
-  ...db.product.toHandlers('rest'), // mock api를 mswjs로 교체
+  // mock api를 mswjs/data로 교체
+  ...db.product.toHandlers('rest'),
+  ...db.category.toHandlers('rest'),
 ];

567cb1ad42ea385c539d3e87c5e0da76fd0d80c8
jinoc <choiae369@gmail.com>
2024-07-30 20:52:25 +0900

    BrowseProductsPage 테스트 컴포넌트 리팩토링
    - mocks/utils.ts에 simulateDelay, simulateError 함수 추가
    - renderComponent 함수의 return에 getProductSkeleton,
      getCategoriesSkeleton, getCategoriesComboBox 함수 추가
    - 그에 따른 테스트 코드 변경

--- tests/components/BrowseProductsPage.test.tsx
+++ tests/components/BrowseProductsPage.test.tsx
@@ -5,12 +5,11 @@ import {
 } from '@testing-library/react';
 import BrowseProducts from '../../src/pages/BrowseProductsPage';
 import { Theme } from '@radix-ui/themes';
-import { server } from '../mocks/server';
-import { delay, http, HttpResponse } from 'msw';
 import { Category, Product } from '../../src/entities';
 import { db } from '../mocks/db';
 import { CartProvider } from '../../src/providers/CartProvider';
 import userEvent from '@testing-library/user-event';
+import { simulateDelay, simulateError } from '../utils';
 
 describe('BrowseProductsPage', () => {
   const categories: Category[] = [];
@@ -44,82 +43,66 @@ describe('BrowseProductsPage', () => {
         </Theme>
       </CartProvider>
     );
+
+    return {
+      getProductsSkeleton: () =>
+        screen.queryByRole('progressbar', { name: /products/i }),
+
+      getCategoriesSkeleton: () =>
+        screen.queryByRole('progressbar', {
+          name: /categories/i,
+        }),
+
+      getCategoriesComboBox: () => screen.queryByRole('combobox'),
+    };
   };
 
   it('should show a loading skeleton when fetching categories', () => {
-    // categories fetching
-    server.use(
-      http.get('/categories', async () => {
-        await delay();
-        return HttpResponse.json([]);
-      })
-    );
+    simulateDelay('/categories');
 
-    renderComponent();
+    const { getCategoriesSkeleton } = renderComponent();
 
     // skeleton을 감싸는 role이 progressbar인 div를 찾음
-    expect(
-      screen.getByRole('progressbar', {
-        name: /categories/i,
-      })
-    ).toBeInTheDocument();
+    expect(getCategoriesSkeleton()).toBeInTheDocument();
   });
 
   it('should hide the loading skeleton after categories are fetched', async () => {
-    renderComponent();
+    const { getCategoriesSkeleton } = renderComponent();
 
     // skeleton을 감싸는 role이 progressbar인 div가 사라지는지 확인
-    await waitForElementToBeRemoved(() =>
-      screen.queryByRole('progressbar', {
-        name: /categories/i,
-      })
-    );
+    await waitForElementToBeRemoved(getCategoriesSkeleton);
   });
 
   it('should show a loading skeleton when fetching products', async () => {
-    // products fetching
-    server.use(
-      http.get('/products', async () => {
-        await delay();
-        return HttpResponse.json([]);
-      })
-    );
+    simulateDelay('/products');
 
-    renderComponent();
+    const { getProductsSkeleton } = renderComponent();
 
     // skeleton을 감싸는 role이 progressbar인 div를 찾음
-    expect(
-      screen.getByRole('progressbar', { name: /products/i })
-    ).toBeInTheDocument();
+    expect(getProductsSkeleton()).toBeInTheDocument();
   });
 
   it('should hide the loading skeleton after products are fetched', async () => {
-    renderComponent();
+    const { getProductsSkeleton } = renderComponent();
 
     // skeleton을 감싸는 role이 progressbar인 div가 사라지는지 확인
-    await waitForElementToBeRemoved(() =>
-      screen.queryByRole('progressbar', { name: /products/i })
-    );
+    await waitForElementToBeRemoved(getProductsSkeleton);
   });
 
   it('should not render an error if categories cannot be fetched', async () => {
-    server.use(http.get('/categories', () => HttpResponse.error()));
+    simulateError('/categories');
 
-    renderComponent();
+    const { getCategoriesSkeleton, getCategoriesComboBox } = renderComponent();
 
     // loading indicator가 사라진 뒤
-    await waitForElementToBeRemoved(() =>
-      screen.queryByRole('progressbar', { name: /products/i })
-    );
+    await waitForElementToBeRemoved(getCategoriesSkeleton);
 
     expect(screen.queryByText(/error/i)).not.toBeInTheDocument(); // error란 텍스트가 없고
-    expect(
-      screen.queryByRole('combobox', { name: /category/i })
-    ).not.toBeInTheDocument(); // select가 없는지
+    expect(getCategoriesComboBox()).not.toBeInTheDocument(); // select가 없는지
   });
 
   it('should render an error if products cannot be fetched', async () => {
-    server.use(http.get('/products', () => HttpResponse.error()));
+    simulateError('/products');
 
     renderComponent();
 
@@ -127,16 +110,19 @@ describe('BrowseProductsPage', () => {
   });
 
   it('should render categories', async () => {
-    renderComponent();
+    const { getCategoriesSkeleton, getCategoriesComboBox } = renderComponent();
+
+    await waitForElementToBeRemoved(getCategoriesSkeleton);
 
     // getByRole을 즉시 찾지만 findByRole은 비동기적으로 요소를 찾음
     // (동적으로 생성되거나 비동기적으로 로딩되는 요소 찾을 때 씀)
     // 시간이 지나도 요소를 찾지 못하면 TimeoutError를 발생
-    const combobox = await screen.findByRole('combobox');
+    // const combobox = await screen.findByRole('combobox');
+    const combobox = getCategoriesComboBox();
     expect(combobox).toBeInTheDocument();
 
     const user = userEvent.setup();
-    await user.click(combobox);
+    await user.click(combobox!);
 
     // 기본으로 렌더링되는 옵션인 All 옵션이 렌더링 되는지 확인
     expect(screen.getByRole('option', { name: /all/i })).toBeInTheDocument();
@@ -149,11 +135,9 @@ describe('BrowseProductsPage', () => {
   });
 
   it('should render products', async () => {
-    renderComponent();
+    const { getProductsSkeleton } = renderComponent();
 
-    await waitForElementToBeRemoved(() =>
-      screen.getByRole('progressbar', { name: /products/i })
-    );
+    await waitForElementToBeRemoved(getProductsSkeleton);
 
     products.forEach((product) => {
       expect(screen.getByText(product.name)).toBeInTheDocument(); // 상품이 렌더링되는지 확인

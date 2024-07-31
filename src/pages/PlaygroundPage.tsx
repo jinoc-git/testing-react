4d0af6d3a4772d1a35f7893a39291b10f73d623c
jinoc <choiae369@gmail.com>
2024-07-31 22:02:17 +0900

    ProductForm initial data 테스트 추가

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -5,11 +5,13 @@
 // import ToastDemo from '../components/ToastDemo';
 // import OrderStatusSelector from '../components/OrderStatusSelector';
 import ProductForm from '../components/ProductForm';
+import { ProductFormData } from '../validationSchemas/productSchema';
 // import BrowseProducts from './BrowseProductsPage';
 // import TermsAndConditions from '../components/TermsAndConditions';
 
 const PlaygroundPage = () => {
   // const longText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, impedit, quasi odit, vel sit corrupti repudiandae at delectus iure ex rerum veritatis voluptatum sed incidunt pariatur necessitatibus! Quam quas et ducimus illum iure quidem in repellat quo minima obcaecati provident velit commodi recusandae perferendis explicabo nihil maiores inventore, quibusdam ipsum beatae, minus facilis repudiandae nulla excepturi! Doloribus quae accusantium qui vel? Quo id alias dolore molestias assumenda qui consequuntur iusto esse? Itaque porro molestias sunt doloribus quia magni quo nihil, odit nulla officia commodi accusantium quisquam maxime laudantium? Architecto error dolorum nobis, corporis ad explicabo in quae sint minima officiis.';
+
   return (
     <>
       <ProductForm />
--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import ProductForm from '../../src/components/ProductForm';
 import AllProviders from '../AllProviders';
+import { Category, Product } from '../../src/entities';
+import { db } from '../mocks/db';
 
 describe('ProductForm', () => {
+  let category: Category;
+
+  beforeAll(() => {
+    category = db.category.create();
+  });
+
+  afterAll(() => {
+    db.category.delete({ where: { id: { equals: category.id } } });
+  });
+
   it('should render form fields', async () => {
     render(<ProductForm onSubmit={vi.fn()} />, { wrapper: AllProviders });
 
@@ -15,5 +27,33 @@ describe('ProductForm', () => {
     expect(
       screen.getByRole('combobox', { name: /category/i })
     ).toBeInTheDocument(); // category 선택 select 확인
+  });
+
+  it('should populate form fields when editing a product', async () => {
+    const product: Product = {
+      id: 1,
+      name: 'Bread',
+      price: 10,
+      categoryId: category.id,
+    };
+
+    render(<ProductForm product={product} onSubmit={vi.fn()} />, {
+      wrapper: AllProviders,
+    });
+
+    await screen.findByRole('form');
+
+    // default value가 product.name이 맞는지 확인
+    expect(screen.getByPlaceholderText(/name/i)).toHaveValue(product.name);
+
+    // default value가 product.price가 맞는지 확인
+    expect(screen.getByPlaceholderText(/price/i)).toHaveValue(
+      product.price.toString()
+    );
+
+    // select의 default value는 props로 전달된 상품의 categoryId이기 때문에 text는 category.name이다
+    expect(
+      screen.getByRole('combobox', { name: /category/i })
+    ).toHaveTextContent(category.name);
   });
 });

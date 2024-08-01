c6f86d0f543467a081559c4cb7066fcffd73426e
jinoc <choiae369@gmail.com>
2024-08-01 19:36:00 +0900

    ProductForm name input validation 테스트 리팩토링
    - name input의 validation 테스트는 min, max 일 때 확인하는데, 같은 테스트 코드를 사용하기 때문에 each를 사용하여
      각 케이스의 매개변수를 받는 방식으로 테스트 코드 변경

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -73,19 +73,30 @@ describe('ProductForm', () => {
     expect(nameInput).toHaveFocus(); // autoFocus가 잘 되는지 확인
   });
 
-  it('should display an error if name is missing', async () => {
-    const { waitForFormToLoad } = renderComponent();
+  it.each([
+    { scenario: 'missing', errorMessage: /required/i }, // name input이 빈 값일 때
+    {
+      scenario: 'longer than 255 characters', // name input의 value가 255글자 초과일 때
+      name: 'a'.repeat(256),
+      errorMessage: /255/i,
+    },
+  ])(
+    'should display an error if name is $scenario', // name input의 validation 체크 (min1, max255)
+    async ({ name, errorMessage }) => {
+      const { waitForFormToLoad } = renderComponent();
 
-    const form = await waitForFormToLoad();
-    const user = userEvent.setup();
-    await user.type(form.priceInput, '10'); // price input에 10을 입력하고
-    await user.click(form.categoryInput); // category select button을 클릭
-    const options = screen.getAllByRole('option'); // select options들
-    await user.click(options[0]); // 첫 번째 카테고리 선택
-    await user.click(form.submitButton); // submit 버튼 클릭
+      const form = await waitForFormToLoad();
+      const user = userEvent.setup();
+      if (name !== undefined) await user.type(form.nameInput, name); // name input에 값이 있을 때 (255 글자 초과인 케이스일 때)
+      await user.type(form.priceInput, '10'); // price input에 10을 입력하고
+      await user.click(form.categoryInput); // category select button을 클릭
+      const options = screen.getAllByRole('option'); // select options들
+      await user.click(options[0]); // 첫 번째 카테고리 선택
+      await user.click(form.submitButton); // submit 버튼 클릭
 
-    const errer = screen.getByRole('alert'); // name은 필수 값이라 error 메시지가 표시 (에러 메시지 div의 role은 alert)
-    expect(errer).toBeInTheDocument(); // 에러 메시지가 렌더링 되는지
-    expect(errer).toHaveTextContent(/required/i); // 에러 메시지가 required 텍스트를 포함하는지
-  });
+      const errer = screen.getByRole('alert'); // 에러 메시지 div의 role은 alert임
+      expect(errer).toBeInTheDocument(); // 에러 메시지가 렌더링 되는지
+      expect(errer).toHaveTextContent(errorMessage); // 에러 메시지가 errorMessage 텍스트를 포함하는지 (인자로 받은)
+    }
+  );
 });

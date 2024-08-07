0935f1610633c40f78a23823f6bc0ad934022b21
jinoc <choiae369@gmail.com>
2024-08-08 00:01:13 +0900

    Router 테스트에 상품 상세 route 테스트 추가

--- tests/components/Router.test.tsx
+++ tests/components/Router.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { navigateTo } from '../utils';
+import { db } from '../mocks/db';
 
 describe('Router', () => {
   it('should render the home page for /', () => {
@@ -14,5 +15,18 @@ describe('Router', () => {
     expect(
       screen.getByRole('heading', { name: /products/i })
     ).toBeInTheDocument();
+  });
+
+  it('should render the product details page for /products/:id', async () => {
+    const product = db.product.create(); // 상품 생성
+
+    navigateTo(`/products/${product.id}`); // ProductDetailPage로 이동
+
+    // 상품의 정보를 불러오기에 비동기로 찾는 findByRole 사용
+    expect(
+      await screen.findByRole('heading', { name: product.name })
+    ).toBeInTheDocument(); // 상품 이름이 h1에 있는지 확인
+
+    db.product.delete({ where: { id: { equals: product.id } } }); // 생성한 상품 삭제
   });
 });

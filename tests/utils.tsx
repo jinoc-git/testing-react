df4583c65791aece9c22a9955536003e116d6450
jinoc <choiae369@gmail.com>
2024-08-07 23:56:16 +0900

    Router 테스트 리팩토링
    - utils.ts -> utils.tsx로 변경
    - utils.tsx에 path를 인자로 받는 navigateTo 함수 추가
    - Router.test.tsx에 navigateTo 함수 적용

--- tests/utils.tsx
+++ tests/utils.tsx
@@ -1,6 +1,9 @@
 import { delay, http, HttpResponse } from 'msw';
 import { server } from './mocks/server';
 import { useAuth0, User } from '@auth0/auth0-react';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import routes from '../src/routes';
+import { render } from '@testing-library/react';
 
 export const simulateDelay = (endpoint: string) => {
   server.use(
@@ -34,4 +37,13 @@ export const mockAuthState = (authState: AuthState) => {
     logout: vi.fn(),
     handleRedirectCallback: vi.fn(),
   });
+};
+
+export const navigateTo = (path: string) => {
+  // 메모리 상에서 라우팅 관리하는 createMemoryRouter 사용
+  const router = createMemoryRouter(routes, {
+    initialEntries: [path], // 인자로 받는 path를 초기 route로 설정
+  });
+
+  render(<RouterProvider router={router} />);
 };
